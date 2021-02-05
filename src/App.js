3e0333abd4b2721517b758071da9a6dfd8b002cd
gabriella-guimaraes <gabigara13@gmail.com>
2021-02-05 17:50:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './components/User';
 
-function App() {
+const users = [
+  {
+    name: 'Isa',
+    description: 'Gosta de sorvete'
+  },
+  {
+    name: 'Cíntia',
+    description: 'Gosta de goiabada cascão'
+  },
+  {
+    name: 'Isa',
+    description: 'Gosta de mousse de chocolate'
+  }
+];
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Oficina de React</h1>
+      <User name={users[0].name} description={users[0].description}/>
+      <input placeholder="Qual o seu sabor favorito?"></input>
+      <User name={users[1].name} description={users[1].description}/>
+      <User name={users[2].name} description={users[2].description}/>
     </div>
   );
 }
---
+++ src/components/User.js
@@ -0,0 +1,27 @@
+const User = ({name, description}) => {
+    return (
+        <div>
+          <p>{name}</p>
+          <p>{description}</p>
+        </div>
+    );
+  }
+// const User = (prop) => {
+//   return (
+//       <div>
+//         <p>{props.name}</p>
+//         <p>{props.description}</p>
+//       </div>
+//   );
+// }
+
+export default User;
+
+// </div>
+// <div>
+//   <p>Cíntia</p>
+//   <p>Gosta de goiabada cascão</p>
+// </div>
+// <div>
+//   <p>Isa</p>
+//   <p>Gosta de mousse de chocolate</p>
